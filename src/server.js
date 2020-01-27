5b406bdae7d601e5cb380cf3e2e5ab00740a2066
Bernardo Aquino <bernardoaquinoc@gmail.com>
2020-01-27 16:42:30 -0300

    Documentation

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
  * @swagger
  * /subjects:
  *  get:
- *    description: Use to request all students
+ *    description: Use to request all subjects
  *    responses:
  *      '200':
  *        description: A successful response
