9202e6c9cdb6a3260d680c29160018e124536e50
Bernardo Aquino <bernardoaquinoc@gmail.com>
2020-02-09 17:09:06 -0300

    Documentation update

--- src/app/models/Subscription.js
+++ src/app/models/Subscription.js
@@ -1,46 +1,46 @@
-const mongoose = require('mongoose');
-const mongoosastic = require('mongoosastic');
+const mongoose = require("mongoose");
+const mongoosastic = require("mongoosastic");
 
-const Student = require('./Student');
-const HelpRequest = require('./HelpRequest');
+const Student = require("./Student");
+const HelpRequest = require("./HelpRequest");
 
 const elasticConnection = require("../../config/elasticConnection.json");
 
 const SubscriptionSchema = new mongoose.Schema({
-    candidate: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Student',
-        required: false,
-        es_indexed: true,
-        es_type: 'nested',
-        es_include_in_parent: true,
-    },
-    help_request: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'HelpRequest',
-        required: true,
-        es_indexed: true,
-        es_type: 'nested',
-        es_include_in_parent: true,
-    },
-    status: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    }
+  candidate: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Student",
+    required: false,
+    es_indexed: true,
+    es_type: "nested",
+    es_include_in_parent: true
+  },
+  help_request: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Request",
+    required: true,
+    es_indexed: true,
+    es_type: "nested",
+    es_include_in_parent: true
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-elasticConnection['populate'] = [
-    {path: 'candidate'},
-    {path: 'help_request'},
-]
+elasticConnection["populate"] = [
+  { path: "candidate" },
+  { path: "help_request" }
+];
 
 SubscriptionSchema.plugin(mongoosastic, elasticConnection);
 
-const Model = mongoose.model('Subscription', SubscriptionSchema);
-Model.synchronize({}, {saveOnSynchronize: true});
+const Model = mongoose.model("Subscription", SubscriptionSchema);
+Model.synchronize({}, { saveOnSynchronize: true });
 
 module.exports = Model;
