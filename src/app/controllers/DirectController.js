e70e8f11ca1386e2aed6eac2b0f836360d699df7
Hugo Brandão <hugo.h.b@hotmail.com>
2020-02-13 20:31:39 -0300

    DirectController implementation

--- src/app/controllers/DirectController.js
+++ src/app/controllers/DirectController.js
@@ -5,18 +5,57 @@ const Direct = require('../models/Direct');
 
 module.exports = {
     async index(request, response) {
+        const { helper } = request.params.id;
 
+        const directs = await Direct.find({ helper });
+        
+        return response.status(200).json(directs);
     },
     async show(request, response) {
+        const { _id } = request.params.id;
 
+        const direct = await Direct.findById({ _id });
+        
+        return response.status(200).json(direct);
     },
     async store(request, response) {
+        const { ...data } = request.body;
 
+        try{
+            const direct = await Direct.create({ data });
+            return request.status(201).json(direct);
+        } catch (error) {
+            return request.status(400).json({
+                message: 'Unable to create direct message.',
+                error
+            });
+        }
     },
     async update(request, response) {
-
+        const { _id } = request.params;
+        const { ...data } = request.body;
+        
+        try {
+            const direct = await Direct.findByIdAndUpdate({ _id }, { data });
+            return request.status(204).json({direct});
+        } catch (error) {
+            return request.status(400).json({
+                message: 'Unable to update direct message.',
+                error
+            });
+        }
     },
     async destroy(request, response) {
+        const { _id } = request.params;
 
+        try {
+            const direct = await Direct.findByIdAndDelete({ _id });
+            return request.status(204).json({direct});
+        } catch (error) {
+            return request.status(400).json({
+                message: 'Unable to delete direct message',
+                error
+            });
+        }
     },
 }
--- src/routes/direct.routes.js
+++ src/routes/direct.routes.js
@@ -3,7 +3,7 @@ const directController = require('../app/controllers/DirectController');
 
 const directRoutes = Router();
 
-directRoutes.get('/directs', directController.index);
+directRoutes.get('/directs/:id', directController.index);
 directRoutes.get('/directs/:id', directController.show);
 directRoutes.post('/directs', directController.store);
 directRoutes.put('/directs/:id', directController.update);
